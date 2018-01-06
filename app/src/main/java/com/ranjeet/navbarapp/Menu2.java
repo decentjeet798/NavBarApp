f49b9b1e11aebb3dc3661017dba34187fbe2f362
decentjeet798 <decentjeet798@users.noreply.github.com>
2018-01-06 16:01:54 +0530

    Initial commit

---
+++ app/src/main/java/com/ranjeet/navbarapp/Menu2.java
@@ -0,0 +1,29 @@
+package com.ranjeet.navbarapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 1/6/2018.
+ */
+
+public class Menu2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_menu_2, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 2");
+    }
+}
